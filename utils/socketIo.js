e357c666bbf7e25e8f3e4469f8dcd238b6effc06
Hongyu Li <821916743@qq.com>
2024-04-03 18:41:46 +0800

    UDP: socket连接需要token

--- utils/socketIo.js
+++ utils/socketIo.js
@@ -1,5 +1,5 @@
 const socketIo = require('socket.io');
-
+const { verifyToken }  = require('./jwt');
 
 module.exports = function initSocket(server) {
     const io = socketIo(server, {
@@ -13,21 +13,37 @@ module.exports = function initSocket(server) {
       
     // 监听连接事件
     io.on('connection', socket => {
-        console.log('用户已连接');
-
+        const token = socket.handshake.query.token;
+        // 如果 token 不存在，返回 401 错误
+        if (!token) {
+            console.log(`${new Date().toLocaleString()} 未提供token，拒绝连接`);
+            // 返回认证失败信息给客户端
+            socket.emit('authError', { message: 'Token 验证失败，拒绝连接' });
+            socket.disconnect(true);
+            return;
+        }
+        const decodedToken = verifyToken(token);
+        if (!decodedToken) {
+            // 返回认证失败信息给客户端
+            console.log(`${new Date().toLocaleString()} token错误拒绝连接`);
+            socket.emit('authError', { message: 'Token 验证失败，拒绝连接' });
+            socket.disconnect(true);
+            return;
+        }
+        console.log(`${new Date().toLocaleString()} 用户 ${decodedToken.username} 已连接 `);
         socket.on('message', function (data) {
-            console.log('服务端收到 : ', data);
+            console.log(`${new Date().toLocaleString()} 服务端收到 : `, data);
             socket.send('你好客户端, ' + data);
         });
      
         //监听自定义事件
         socket.on('myevent', function (data) {
-            console.log('客户端发送了一个自定义事件', data);
+            console.log(`${new Date().toLocaleString()} 客户端发送了一个自定义事件 `, data);
         });
 
         // 监听断开连接事件
         socket.on('disconnect', () => {
-            console.log(`已断开连接`);
+            console.log(`${new Date().toLocaleString()} 用户 ${decodedToken.username} 已断开连接`);
         });
     });
     return io   
